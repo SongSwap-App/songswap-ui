bef7c3f0dce37c1d5ffab2319e1211a400f51cf2
kvand <kvand@ORDI>
2024-03-20 14:03:48 +0100

    add skip negotiation

--- src/components/SignalR.js
+++ src/components/SignalR.js
@@ -1,7 +1,10 @@
 import * as signalR from '@microsoft/signalr';
 
 const connection = new signalR.HubConnectionBuilder()
-    .withUrl('hub') // Specify the URL of your SignalR hub
+    .withUrl('hub', {
+      skipNegotiation: true,
+      transport: signalR.HttpTransportType.WebSockets
+    }) // Specify the URL of your SignalR hub
     .build();
 
 connection.start().catch(err => console.error('SignalR connection error: ', err));
